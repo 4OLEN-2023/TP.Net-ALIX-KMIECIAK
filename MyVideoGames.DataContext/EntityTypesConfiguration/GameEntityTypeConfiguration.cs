49c17cdc77b4538d1862eebec697a3e30b570697
Clement Fevre <cfevre@nouveal.com>
2023-12-14 00:03:29 +0100

    Missing file

--- MyVideoGames.DataContext/EntityTypesConfiguration/GameEntityTypeConfiguration.cs
+++ MyVideoGames.DataContext/EntityTypesConfiguration/GameEntityTypeConfiguration.cs
@@ -9,7 +9,7 @@ public class GameEntityTypeConfiguration : IEntityTypeConfiguration<Game>
     public void Configure(EntityTypeBuilder<Game> builder)
     {
         builder.HasKey(item => item.Id);
-
+        
         builder.HasOne(item => item.Platform)
             .WithMany(item => item.RelatedGames)
             .HasForeignKey(item => item.PlatformId);
