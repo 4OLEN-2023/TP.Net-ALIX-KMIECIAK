b5fff109ff2d1bc4c500d9d8d0279de45ceec8c4
sy-alx <84022109+sy-alx@users.noreply.github.com>
2024-01-08 15:08:40 +0100

    ajout travail a rendre tp3 partie 19 - 3jours

---
+++ 4OLEN-TP-2023/4OLEN-TP-2023/MyVideoGames.WebUI.UnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace MyVideoGames.WebUI.UnitTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
