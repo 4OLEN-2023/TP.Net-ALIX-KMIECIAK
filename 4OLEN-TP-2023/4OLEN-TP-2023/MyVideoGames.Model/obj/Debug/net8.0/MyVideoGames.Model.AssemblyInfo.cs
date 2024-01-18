372f20334977d83c71f3f561517ab06beac36669
sy-alx <84022109+sy-alx@users.noreply.github.com>
2024-01-18 13:27:06 +0100

    TP5

--- 4OLEN-TP-2023/4OLEN-TP-2023/MyVideoGames.Model/obj/Debug/net8.0/MyVideoGames.Model.AssemblyInfo.cs
+++ 4OLEN-TP-2023/4OLEN-TP-2023/MyVideoGames.Model/obj/Debug/net8.0/MyVideoGames.Model.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyVideoGames.Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5fff109ff2d1bc4c500d9d8d0279de45ceec8c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyVideoGames.Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyVideoGames.Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
